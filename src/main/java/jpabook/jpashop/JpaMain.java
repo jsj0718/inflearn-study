5514f088f65aa57c5dab554f05d2998acb4d3a4d
jsj0718 <jeongsejin718@gmail.com>
2022-01-21 00:57:09 +0900

    jpql 실습

--- src/main/java/jpabook/jpashop/JpaMain.java
+++ src/main/java/jpabook/jpashop/JpaMain.java
@@ -1,11 +1,10 @@
 package jpabook.jpashop;
 
 import jpabook.jpashop.domain.Book;
+import jpabook.jpashop.domain.Item;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
+import javax.persistence.*;
+import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -21,6 +20,8 @@ public class JpaMain {
             book.setAuthor("김영한");
 
             em.persist(book);
+
+            List<Item> result = em.createQuery("select i from Item i where type(i) = Book", Item.class).getResultList();
 
             tx.commit();
         } catch (Exception e) {
