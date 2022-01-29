af06571529db6cf2437fb4cda9d73108e4d2b2f6
jsj0718 <jeongsejin718@gmail.com>
2022-01-29 19:25:23 +0900

    도메인 설계

---
+++ jpashop/src/main/java/jpabook/jpashop/domain/Address.java
@@ -0,0 +1,25 @@
+package jpabook.jpashop.domain;
+
+import lombok.Getter;
+
+import javax.persistence.Embeddable;
+
+@Embeddable
+@Getter
+//값 타입은 변경 불가능하게 설계해야 하므로 Setter X
+public class Address {
+
+    private String city;
+    private String street;
+    private String zipcode;
+
+    //JPA 스펙 상 기본 생성자 생성
+    protected Address() {
+    }
+
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
+}
